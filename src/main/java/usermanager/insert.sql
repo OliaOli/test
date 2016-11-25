6ab5073e950f22ca2c204e6bf78180d39de9546d
OliaOli <freldo_test@mail.ru>
2016-11-25 23:26:45 +0200

    Update insert.sql

--- src/main/java/usermanager/insert.sql
+++ src/main/java/usermanager/insert.sql
@@ -6,3 +6,9 @@ CREATE TABLE IF NOT EXISTS `User` (
   `createdDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
+
+INSERT INTO `User` (`id`, `name`, `age`, `isAdmin`, `createdDate`) VALUES
+  (1, 'user1', 21, 0, '2015-11-25 10:05:12'),
+  (2, 'user2', 22, 0, '2015-11-25 10:06:25'),
+  (3, 'user3', 23, 0, '2015-11-25 10:12:47'),
+  (4, 'user4', 21, 1, '2016-11-25 10:20:30');
